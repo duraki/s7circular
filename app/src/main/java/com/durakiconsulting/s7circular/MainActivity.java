114237167fb29729e4479e9f50dec33d9509fa82
duraki <duraki@linuxmail.org>
2021-02-18 10:09:52 +0100

    add some stuff

--- app/src/main/java/com/durakiconsulting/s7circular/MainActivity.java
+++ app/src/main/java/com/durakiconsulting/s7circular/MainActivity.java
@@ -3,6 +3,7 @@ package com.durakiconsulting.s7circular;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
